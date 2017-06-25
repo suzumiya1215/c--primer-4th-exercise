418a93ca87ddbdf6ad78de952e8fbd7ef0974717
suzumiya1215 <1030011936@qq.com>
2017-06-25 10:12:36 +0800

    c++ primer exercise 10.26 ver 1.0

---
+++ c++ primer exercise 10.26.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+using namespace std;
+
+int main()
+{
+	multimap<string, string> store;
+	store.insert(make_pair(string("a"), string("a1")));
+	store.insert(make_pair(string("a"), string("a2")));
+	store.insert(make_pair(string("a"), string("a3")));
+	store.insert(make_pair(string("a"), string("a4")));
+	store.insert(make_pair(string("b"), string("b1")));
+	store.insert(make_pair(string("b"), string("b2")));
+	store.insert(make_pair(string("b"), string("b3")));
+	store.insert(make_pair(string("b"), string("b4")));
+	
+	string search;
+	cin >> search;
+	typedef multimap<string, string>::size_type sz;
+	sz sz_count = store.count(search);
+	multimap<string, string>::iterator iter1 = store.find(search);
+	store.erase(search);
+
+	multimap<string, string>::iterator iter2 = store.begin();
+	while (iter2 != store.end())
+	{
+		cout << iter2->first << '\t' <<iter2->second<< endl;
+		++iter2;
+	}
+
+	system("pause");
+	return 0;
+}
