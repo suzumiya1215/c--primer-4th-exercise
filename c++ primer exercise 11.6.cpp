97fa8d32e8fab0d8b00cc70f05219e00e4df9197
suzumiya1215 <1030011936@qq.com>
2017-07-01 18:21:41 +0800

    c++ primer exercise 11.6 ver 1.0

---
+++ c++ primer exercise 11.6.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	list<int> a = { 1,2,3,4,5,6,7,8 };
+	fill_n(a.begin(), 8, 0);
+	for (list<int>::iterator iter = a.begin(); iter != a.end(); ++iter)
+		cout << *iter << endl;
+
+	system("pause");
+	return 0;
+}
