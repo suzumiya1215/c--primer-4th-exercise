e8368e5d45ecd48f31ccafdfc58df92d8889e3be
suzumiya1215 <1030011936@qq.com>
2017-07-03 19:35:08 +0800

    c++ primer exercise 11.17

---
+++ c++ primer exercise 11.17.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	vector<int> vec = { 1,2,3,4,5,6,7,8 };
+	istream_iterator<string> in_iter(cin), end;
+	vector<int> vec(in_iter, end);
+
+	system("pause");
+	return 0;
+}
