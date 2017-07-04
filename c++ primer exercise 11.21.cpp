0b2c2cdffb7eac5e243fa39552296ebf8a6e4833
suzumiya1215 <1030011936@qq.com>
2017-07-04 23:05:15 +0800

    c++ primer exercise 11.21

---
+++ c++ primer exercise 11.21.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	list<int> lis = { 1,2,3,4,5,6,7,8,0,1,2,3,0,4,5 };
+	list<int>::reverse_iterator iter = find(lis.rbegin(), lis.rend(), 0);
+	cout << *iter << '\n';
+
+	system("pause");
+	return 0;
+}
