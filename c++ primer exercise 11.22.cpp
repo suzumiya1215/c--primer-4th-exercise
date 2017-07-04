e30e30d3269bd081d2165c495c27e6e6d7b6a897
suzumiya1215 <1030011936@qq.com>
2017-07-04 23:44:52 +0800

    c++ primer exercise 11.22

---
+++ c++ primer exercise 11.22.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	vector<int> vec = { 1,2,3,4,5,6,7,8,9,10 };
+	list<int> lis(vec.rbegin() + 3, vec.rbegin() + 8);
+	for (list<int>::iterator iter = lis.begin(); iter != lis.end(); ++iter)
+		cout << *iter << '\n';
+
+	system("pause");
+	return 0;
+}
