e4e90ca0efed5a7ac9848c310052b32d4f5b1e3a
suzumiya1215 <1030011936@qq.com>
2017-07-04 22:58:02 +0800

    c++ primer exercise 11.20

---
+++ c++ primer exercise 11.20.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	vector<int> vec = { 1,2,3,4,5,6,7,8 };
+	for (vector<int>::reverse_iterator iter = vec.rbegin(); iter != vec.rend(); ++iter)
+		cout << *iter << '\n';
+	cout << '\n';
+	for (vector<int>::iterator iter= vec.end()-1;iter!=vec.begin();--iter)
+		cout << *iter << '\n';
+	cout << *vec.begin() << '\n';
+	system("pause");
+	return 0;
+}
