c21f1c95da77aafe4802c8391d0ef9df001eb019
suzumiya1215 <1030011936@qq.com>
2017-07-04 23:48:52 +0800

    c++ primer exercise 11.22

--- c++ primer exercise 11.22.cpp
+++ c++ primer exercise 11.22.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
 	vector<int> vec = { 1,2,3,4,5,6,7,8,9,10 };
-	list<int> lis(vec.rbegin() + 3, vec.rbegin() + 8);
+	list<int> lis(vec.rbegin() + 3, vec.rbegin() + 8);//list初始化技巧
 	for (list<int>::iterator iter = lis.begin(); iter != lis.end(); ++iter)
 		cout << *iter << '\n';
 
