cf45100ba3287cbc8b6e7cd811666a3442927276
suzumiya1215 <1030011936@qq.com>
2017-09-03 22:57:03 +0800

    c++ primer exercise 12.22 & 12.23

---
+++ c++ primer exercise 12.22.cpp
@@ -0,0 +1,19 @@
+﻿struct x
+{
+	x(int i, int j) :base(i), rem(base%j) {}
+	int rem, base;
+};
+
+struct n
+{
+	n(int i) {}
+};
+
+class c
+{
+public:
+	c() : i(0), d(i) {}
+private:
+	int i;
+	n d;
+};
